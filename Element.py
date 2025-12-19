98487971d3f5a44881790b32031e27d1b18469af
Darknight47 <farhadgunners@gmail.com>
2025-12-19 12:09:51 +0100

    implementation

---
+++ Element.py
@@ -0,0 +1,50 @@
+"""
+
+------------------------------------------------------- Link for the challenge: https://codeforces.com/problemset/problem/177/A2 --------------------------------------------
+
+The Smart Beaver from ABBYY got hooked on square matrices. Now he is busy studying an n × n size matrix, where n is odd. The Smart Beaver considers the following matrix elements good:
+
+Elements of the main diagonal.
+Elements of the secondary diagonal.
+Elements of the "middle" row — the row which has exactly  rows above it and the same number of rows below it.
+Elements of the "middle" column — the column that has exactly  columns to the left of it and the same number of columns to the right of it.
+
+Help the Smart Beaver count the sum of good elements of the given matrix.
+
+Input
+The first line of input data contains a single odd integer n. Each of the next n lines contains n integers aij (0 ≤ aij ≤ 100) separated by single spaces — the elements of the given matrix.
+
+The input limitations for getting 30 points are:
+
+1 ≤ n ≤ 5
+The input limitations for getting 100 points are:
+
+1 ≤ n ≤ 101
+Output
+Print a single integer — the sum of good matrix elements.
+
+Input:
+3
+1 2 3
+4 5 6
+7 8 9
+
+Output:
+45
+"""
+n = int(input())
+matrix = []
+for i in range(n):
+    matrix.append(list(map(int, input().split())))
+ans = 0
+for i in range(n): # row-wise
+    for j in range(n): # column-wise
+        if(i == j): # Main-diagonal
+            ans += matrix[i][j]
+        elif(i + j == n - 1): # secondary-diagnoal
+            ans += matrix[i][j]
+        elif(i == n // 2): # middle row
+            ans += matrix[i][j]
+        elif(j == n // 2): # middle column
+            ans += matrix[i][j]
+print(ans)
