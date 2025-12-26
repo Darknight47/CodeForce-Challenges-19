bbdcdf53f6ff0646e3f5bf2d9ca71c4747548106
Darknight47 <farhadgunners@gmail.com>
2025-12-26 18:46:17 +0100

    brute force, implementation

---
+++ Courtesy.py
@@ -0,0 +1,37 @@
+"""
+
+------------------------------------------------ Link for the challenge: https://codeforces.com/contest/811/problem/A -------------------------------------
+
+At regular competition Vladik and Valera won a and b candies respectively. Vladik offered 1 his candy to Valera. 
+After that Valera gave Vladik 2 his candies, so that no one thought that he was less generous. Vladik for same reason gave 3 candies to Valera in next turn.
+
+More formally, the guys take turns giving each other one candy more than they received in the previous turn.
+
+This continued until the moment when one of them couldn’t give the right amount of candy. Candies, which guys got from each other, they don’t consider as their own. 
+You need to know, who is the first who can’t give the right amount of candy.
+
+Input
+Single line of input data contains two space-separated integers a, b (1 ≤ a, b ≤ 109) — number of Vladik and Valera candies respectively.
+
+Output
+Pring a single line "Vladik’’ in case, if Vladik first who can’t give right amount of candy, or "Valera’’ otherwise.
+
+Input:
+1 1
+
+Output:
+Valera
+"""
+vladik, valera =  map(int, input().split())
+idx = 1
+while True:
+    vladik -= idx
+    idx += 1
+    if(valera < idx):
+        print("Valera")
+        break
+    valera -= idx
+    idx += 1
+    if(vladik < idx):
+        print("Vladik")
+        break
